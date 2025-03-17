d1bfcf4010ef167645c6467c3240bc24fa682bfb
parksoomin <soomin141108@naver.com>
2025-03-17 11:51:40 +0900

    2주차과제제

--- 변수.py
+++ 변수.py
@@ -1,4 +1,16 @@
-a = 19
-b = 29
-c = a + b
-print(a, b, c)
+num1 = 100
+num2 = 200
+resSum = num1 + num2
+print(num1, "+", num2, "=", resSum)
+빼기 = num1 - num2
+print(num1, "-", num2, "=", 빼기)
+곱하기 = num1 * num2
+print(num1, "x", num2, "=", 곱하기)
+나누기 = num1 / num2
+print(num1, "%", num2, "=", 나누기)
+
+str1 = "Hello"
+str2 = "World"
+print(str1*5)
+
+res = resSum - 곱하기
