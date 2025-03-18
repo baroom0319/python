882d409039d9c738b2ace7051f18e920377dfa46
parksoomin <soomin141108@naver.com>
2025-03-19 03:28:33 +0900

    계산기코드드

---
+++ circulrator.py
@@ -0,0 +1,15 @@
+num1 = input("숫자1 ==>")
+num1 = int(num1)
+print(num1)
+num2 = input("숫자2 ==>")
+num2 = int(num2)
+print(num2)
+
+numSum = num1 + num2
+print(num1, "+", num2, "=", numSum)
+빼기 = num1 - num2
+print(num1, "-", num2, "=", 빼기)
+곱하기 = num1 * num2
+print(num1, "x", num2, "=", 곱하기)
+나누기 = num1 / num2
+print(num1, "/", num2, "=", 나누기)
