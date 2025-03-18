30aef61351afbac6655b67268e65fd8953f3dd1d
parksoomin <soomin141108@naver.com>
2025-03-19 03:29:57 +0900

    계산기코드

--- circulrator.py
+++ circulrator.py
@@ -13,3 +13,7 @@ print(num1, "-", num2, "=", 빼기)
 print(num1, "x", num2, "=", 곱하기)
 나누기 = num1 / num2
 print(num1, "/", num2, "=", 나누기)
+제곱 = num1 ** num2
+print(num1, "**", num2, "=", 제곱)
+반제곱 = num1 % num2
+print(num1, "%", num2, "=", 반제곱)
--- inputTest.py
+++ inputTest.py
@@ -7,3 +7,13 @@ print(num2)
 
 numSum = num1 + num2
 print(num1, "+", num2, "=", numSum)
+빼기 = num1 - num2
+print(num1, "-", num2, "=", 빼기)
+곱하기 = num1 * num2
+print(num1, "x", num2, "=", 곱하기)
+나누기 = num1 / num2
+print(num1, "/", num2, "=", 나누기)
+제곱 = num1 ** num2
+print(num1, "**", num2, "=", 제곱)
+반제곱 = num1 % num2
+print(num1, "%", num2, "=", 반제곱)
