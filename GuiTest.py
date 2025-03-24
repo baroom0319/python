a2f5d2c9ebfd452d0f3c945abac234539fba5440
parksoomin <soomin141108@naver.com>
2025-03-24 11:01:02 +0900

    3주차차

--- GuiTest.py
+++ GuiTest.py
@@ -1,3 +1,30 @@
 import tkinter
+
+def click_btn():
+    labelE = tkinter.Label(root, text="동", font=("system", 24))
+    labelW = tkinter.Label(root, text="서", font=("system", 24))
+    labelN = tkinter.Label(root, text="남", font=("system", 24))
+    labelS = tkinter.Label(root, text="북", font=("system", 24))
+
+    labelE.place(x=300, y=200)
+    labelW.place(x=100, y=200)
+    labelN.place(x=200, y=300)
+    labelS.place(x=200, y=100)
+
+    txt = entry.get()
+    str1 = txt
+    labeltxt = tkinter.Label(root, text=str1, font=("system", 24))
+    labeltxt.place(x=250, y=350)
+
 root = tkinter.Tk()
+
+root.title("첫 번째 윈도우")
+root.geometry("800x600")
+
+btn = tkinter.Button(root, text="버튼", font=("system", 10), command=click_btn)
+btn.place(x=200, y=200, width=32, height=32)
+
+entry = tkinter.Entry(width=5)
+entry.place(x=200, y= 350)
+
 root.mainloop()
