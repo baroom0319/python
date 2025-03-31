9d5e0082a6f10cc4e1dea9569b22a0343d91a986
parksoomin <soomin141108@naver.com>
2025-03-31 11:58:36 +0900

    4주차

---
+++ dataType.py
@@ -0,0 +1,18 @@
+var1 = "나는\"파이썬\"을 열공 중입니다."
+print(var1)
+
+print("\n줄바꿈\n연습 ")
+print("\t탭키\t연습")
+print("어떤 글자를 \"강조\"하는 효과1")
+print("어떤 글자를 \'강조\'하는 효과2")
+print("\\\\ 백슬래시 2개 출력")
+
+var2= "나는 파이썬을 열공 중입니다."
+var3= "화이팅"
+
+var4 = var2+var3
+var5 = var3+var2
+
+print(var4)
+print(var5)
+
